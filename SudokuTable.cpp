ca8747f694b8dc7b810543c2087ddc4f6c19292d
Alexander <awittscience@gmail.com>
2021-02-08 13:40:37 -0500

    Create SudokuTable.cpp

---
+++ SudokuTable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "SudokuTable.h"
+#include "SingleBox.h"
+#include "Pair.h"
+
+const int MAXLINE = 18;
+
+using namespace std;
+
+/**
+ *
+ */
+SudokuTable::SudokuTable(const char* inputFile, const char* outputFile) {
+}
